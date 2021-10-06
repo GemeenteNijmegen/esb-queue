cff6b0aa23b6d76079b8dce668292fbaaa3447d8
Martijn van Dijk <m.van.dijk@nijmegen.nl>
2021-10-06 08:25:27 +0000

    new pipeline

--- src/pipeline-stack.ts
+++ src/pipeline-stack.ts
@@ -2,11 +2,16 @@ import * as codecommit from '@aws-cdk/aws-codecommit';
 import * as core from '@aws-cdk/core';
 import * as cdkpipelines from '@aws-cdk/pipelines';
 
+export class EmptyStack extends core.Stack {}
+
+export class EmptyyStack extends core.Stack {}
+
 class emptyStage extends core.Stage {
 
   constructor(scope: core.Construct, id: string, props: core.StageProps) {
     super(scope, id, props);
 
+    new EmptyStack(this, 'empty');
   }
 }
 
@@ -15,6 +20,7 @@ class emptyyStage extends core.Stage {
   constructor(scope: core.Construct, id: string, props: core.StageProps) {
     super(scope, id, props);
 
+    new EmptyyStack(this, 'emptyy');
   }
 }
 
@@ -26,66 +32,29 @@ export class PipelineStack extends core.Stack {
     core.Tags.of(this).add('project', 'esb');
 
     /**
-         * Main repository
-         */
+     * Main repository
+     */
     const repository = new codecommit.Repository(this, 'repository', {
       repositoryName: 'esb-repository',
     });
 
-    if (process.env.BRANCH_NAME === 'acceptance') {
-      const acceptancePipeline = new cdkpipelines.CodePipeline(this, 'acceptancePipeline', {
-        pipelineName: 'esb-pipeline-acceptance',
-        crossAccountKeys: true,
-        synth: new cdkpipelines.ShellStep('Synth', {
-          input: cdkpipelines.CodePipelineSource.codeCommit(repository, 'acceptance'),
-          env: {
-            BRANCH_NAME: '#{SourceVariables.BranchName}',
-          },
-          commands: [
-            'yarn install --frozen-lockfile', //nodig om projen geinstalleerd te krijgen
-            'npx projen build',
-            'npx projen synth',
-          ],
-        }),
-      });
-
-      acceptancePipeline.addStage( new emptyStage(this, 'empty', {
-
+    const pipeline = new cdkpipelines.CodePipeline(this, 'pipeline', {
+      pipelineName: 'esb-pipeline',
+      crossAccountKeys: true,
+      synth: new cdkpipelines.ShellStep('Synth', {
+        input: cdkpipelines.CodePipelineSource.codeCommit(repository, 'main'),
+        commands: [
+          'yarn install --frozen-lockfile', //nodig om projen geinstalleerd te krijgen
+          'npx projen build',
+          'npx projen synth',
+        ],
       }),
-      );
+    });
 
-      acceptancePipeline.addStage( new emptyyStage(this, 'emptyy', {
+    pipeline.addStage( new emptyStage(this, 'empty', {}),
+    );
 
-      }),
-      );
-    }
-
-    if (process.env.BRANCH_NAME === 'production') {
-      const productionPipeline = new cdkpipelines.CodePipeline(this, 'productionPipeline', {
-        pipelineName: 'esb-pipeline-production',
-        crossAccountKeys: true,
-        synth: new cdkpipelines.ShellStep('Synth', {
-          input: cdkpipelines.CodePipelineSource.codeCommit(repository, 'production'),
-          env: {
-            BRANCH_NAME: '#{SourceVariables.BranchName}',
-          },
-          commands: [
-            'yarn install --frozen-lockfile', //nodig om projen geinstalleerd te krijgen
-            'npx projen build',
-            'npx projen synth',
-          ],
-        }),
-      });
-
-      productionPipeline.addStage( new emptyStage(this, 'empty', {
-
-      }),
-      );
-
-      productionPipeline.addStage( new emptyyStage(this, 'emptyy', {
-
-      }),
-      );
-    }
+    pipeline.addStage( new emptyyStage(this, 'emptyy', {}),
+    );
   }
 }
--- test/main.test.ts
+++ test/main.test.ts
@@ -6,6 +6,5 @@ test('Snapshot', () => {
   const app = new App();
   const stack = new PipelineStack(app, 'test');
 
-  expect(stack).not.toHaveResource('AWS::S3::Bucket');
   expect(app.synth().getStackArtifact(stack.artifactId).template).toMatchSnapshot();
 });
