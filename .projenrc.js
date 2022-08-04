f47507364bcf66efa201655ba43136cb9c811694
Marnix Dessing <m.dessing@nijmegen.nl>
2022-08-04 14:04:24 +0200

    fix: wrong upgrade branch

--- .projenrc.js
+++ .projenrc.js
@@ -3,6 +3,11 @@ const { GemeenteNijmegenCdkApp } = require('@gemeentenijmegen/modules-projen');
 const project = new GemeenteNijmegenCdkApp({
   cdkVersion: '2.35.0',
   defaultReleaseBranch: 'main',
+  depsUpgradeOptions: { // Override from GemeenteNijmegenCdkApp as we only use the main branch
+    workflowOptions: {
+      branches: ['main'],
+    },
+  },
   name: 'esb-queue',
   deps: [
     '@gemeentenijmegen/modules-projen',
