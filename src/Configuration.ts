e54089c6537b9c30893d42546e14dc3d2b586896
Marnix Dessing <m.dessing@nijmegen.nl>
2023-08-25 17:28:38 +0200

    feat: prepare for new landingzone branches

--- src/Configuration.ts
+++ src/Configuration.ts
@@ -43,19 +43,46 @@ export interface Configuration {
    */
   esbStageName?: string;
 
+  /**
+   * Flag to indicate that the workload is 
+   * deployed to the new landingzone
+   */
+  isInNewLandingzone?: boolean;
+
 }
 
 export const configurations: { [key: string]: Configuration } = {
+  'development-new-lz': {
+    branchName: 'development-new-lz',
+    codeStarConnectionArn: statics.gnBuildCodeStarConnectionArn,
+    deploymentEnvironment: statics.gnBuildEnvironment,
+    targetEnvironment: statics.gnWebformsDevEnvironment,
+    domainName: 'dev.csp-nijmegen.nl',
+  },
+  'acceptance-new-lz': {
+    branchName: 'acceptance-new-lz',
+    codeStarConnectionArn: statics.gnBuildCodeStarConnectionArn,
+    deploymentEnvironment: statics.gnBuildEnvironment,
+    targetEnvironment: statics.gnWebformsAccpEnvironment,
+    domainName: 'accp.csp-nijmegen.nl',
+  },
+  'main-new-lz': {
+    branchName: 'main-new-lz',
+    codeStarConnectionArn: statics.gnBuildCodeStarConnectionArn,
+    deploymentEnvironment: statics.gnBuildEnvironment,
+    targetEnvironment: statics.gnWebformsProdEnvironment,
+    domainName: 'nijmegen.nl',
+  },
   development: {
     branchName: 'development',
-    codeStarConnectionArn: statics.gnBuildCodeStarConnectionArn,
+    codeStarConnectionArn: statics.codeStarConnectionArn,
     deploymentEnvironment: statics.deploymentEnvironment,
     targetEnvironment: statics.sandboxEnvironment,
     domainName: 'dev.csp-nijmegen.nl',
   },
   acceptance: {
     branchName: 'acceptance',
-    codeStarConnectionArn: statics.gnBuildCodeStarConnectionArn,
+    codeStarConnectionArn: statics.codeStarConnectionArn,
     deploymentEnvironment: statics.deploymentEnvironment,
     targetEnvironment: statics.acceptanceEnvironment,
     domainName: 'accp.csp-nijmegen.nl',
@@ -63,7 +90,7 @@ export const configurations: { [key: string]: Configuration } = {
   },
   main: {
     branchName: 'main',
-    codeStarConnectionArn: statics.gnBuildCodeStarConnectionArn,
+    codeStarConnectionArn: statics.codeStarConnectionArn,
     deploymentEnvironment: statics.deploymentEnvironment,
     targetEnvironment: statics.productionEnvironment,
     domainName: 'nijmegen.nl',
--- src/statics.ts
+++ src/statics.ts
@@ -8,6 +8,11 @@ export class statics {
   static readonly AWS_ACCCOUNT_AUTH_PROD: string = '196212984627';
   static readonly AWS_ACCOUNT_DEPLOYMENT: string = '418648875085';
   static readonly AWS_ACCOUNT_IAM: string = '098799052470';
+  // New lz
+  static readonly AWS_ACCOUNT_GN_BUILD: string = '836443378780';
+  static readonly AWS_ACCOUNT_WEBFORMS_DEV: string = '033598396027';
+  static readonly AWS_ACCOUNT_WEBFORMS_ACCP: string = '338472043295';
+  static readonly AWS_ACCOUNT_WEBFORMS_PROD: string = '147064197580';
 
   static readonly projectName: string = 'esb-queue';
 
@@ -15,6 +20,7 @@ export class statics {
    * Pipeline values
    */
   static readonly codeStarConnectionArn: string = 'arn:aws:codestar-connections:eu-west-1:418648875085:connection/4f647929-c982-4f30-94f4-24ff7dbf9766';
+  static readonly gnBuildCodeStarConnectionArn = 'arn:aws:codestar-connections:eu-central-1:836443378780:connection/9d20671d-91bc-49e2-8680-59ff96e2ab11';
   static readonly projectRepo: string = 'GemeenteNijmegen/esb-queue';
 
   /**
@@ -47,9 +53,26 @@ export class statics {
     region: 'eu-west-1',
   };
 
-  /**
-   * Codestar connection ARN to connect to GitHub.
-   */
-  static readonly gnBuildCodeStarConnectionArn = 'arn:aws:codestar-connections:eu-west-1:418648875085:connection/4f647929-c982-4f30-94f4-24ff7dbf9766';
+  static readonly gnBuildEnvironment = {
+    account: statics.AWS_ACCOUNT_GN_BUILD,
+    region: 'eu-central-1',
+  };
+
+  static readonly gnWebformsDevEnvironment = {
+    account: statics.AWS_ACCOUNT_WEBFORMS_DEV,
+    region: 'eu-central-1',
+  };
+
+  static readonly gnWebformsAccpEnvironment = {
+    account: statics.AWS_ACCOUNT_WEBFORMS_ACCP,
+    region: 'eu-central-1',
+  };
+
+  static readonly gnWebformsProdEnvironment = {
+    account: statics.AWS_ACCOUNT_WEBFORMS_PROD,
+    region: 'eu-central-1',
+  };
+
+  
 
 }
