2c52d44a31d548ed4ae0a5bda54c25ae601d1dd3
Marnix Dessing <m.dessing@nijmegen.nl>
2023-08-25 17:49:50 +0200

    feat: convert iam stack to also work in new lz

--- src/iam-structs/esb-mule-struct.ts
+++ src/iam-structs/esb-mule-struct.ts
@@ -1,25 +1,41 @@
 import * as core from 'aws-cdk-lib';
 import * as iam from 'aws-cdk-lib/aws-iam';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
 
 export interface esbIAMMuleProps extends core.StackProps {
   esbSqsArn: string;
   esfMailNotificationSqsArn: string;
   iamAccountPrincipal: string;
+  isInNewLandingzone?: boolean;
 };
 
 export class esbIAMMule extends Construct {
   constructor(scope: Construct, id: string, props: esbIAMMuleProps) {
     super(scope, id);
 
-    /**
-     * Role: wordt gebruikt door system accounts (Mule), dus geen MFA eisen
-     */
-    const esbSqsMuleRole = new iam.Role(this, 'esb-sqs-mule-role', {
-      roleName: 'esb-sqs-mule',
-      assumedBy: new iam.AccountPrincipal(props.iamAccountPrincipal),
-      description: 'assumable role for Mule to get access to sqs queue',
-    });
+    let esbSqsMuleRole = undefined;
+    if(props.isInNewLandingzone){
+      // In the new lz the iam user lives in the account
+      const user = new iam.User(this, 'esb-sqs-mule-user');
+      const key = new iam.AccessKey(this, 'esb-sqs-mule-user-key', { user });
+      new Secret(this, 'esb-sqs-mule-user-secret', {
+        description: 'Secret key for ESB mule user to poll the esb-queue',
+        secretStringValue: key.secretAccessKey,
+      });
+      esbSqsMuleRole = new iam.Role(this, 'esb-sqs-mule-role', {
+        roleName: 'esb-sqs-mule',
+        assumedBy: user,
+        description: 'assumable role for Mule to get access to sqs queue',
+      });
+    } else {
+      // Role: wordt gebruikt door system accounts (Mule), dus geen MFA eisen
+      esbSqsMuleRole = new iam.Role(this, 'esb-sqs-mule-role', {
+        roleName: 'esb-sqs-mule',
+        assumedBy: new iam.AccountPrincipal(props.iamAccountPrincipal),
+        description: 'assumable role for Mule to get access to sqs queue',
+      });
+    }
 
     const policy = new iam.ManagedPolicy(this, 'esb-sqs-poll', {
       managedPolicyName: 'esb-sqs-poll',
